82e2581d210fdc1b39f0e2ad892d40b272defa88
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-19 11:35:50 +0600

    added three lines basically, but with different form. it will eable adding different types of styles in the end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <div>Delete</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
