e05929084cb4e53b9c079eefeddd9d4daec6d77c
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-19 12:19:49 +0600

    finished ch-02, vid-11-> added code to to Todo component and use that into App.js multiple times

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,12 @@
-function Todo() {}
+function Todo() {
+  return (
+    <div className="card">
+      <h2>Title</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
 
 export default Todo;
